2cbb4da3901610eb314449d1a5c4ae48564667b7
syaedisyafrilnizam <60510081+syaedii@users.noreply.github.com>
2022-06-08 00:20:12 +0900

    Db connection fix

--- server.js
+++ server.js
@@ -49,29 +49,56 @@ const exam = require('./routes/exam');
 
 /* -------------------------------------------- */
 
-//Checking DB conneciton
-db = connection.db;
+//Checking DB connection
+dbMain = connection.dbMain; // main connection
+db = connection.db; // pool
+mysqlConfig = connection.mysqlConfig; // db configurations
 
-function handleDisconnect() {
-  db.connect(function(err) {
-    if(err) {
-        console.log('error when connecting to db: ', err);
-        setTimeout(handleDisconnect, 2000);
-      }
-      console.log("Database connected!");
-    });
+dbMain.connect(function(err) {
+  if (err) throw err;
+  console.log("Database connected!");
+});
 
-    db.on('error', function(err) {
-      console.log('db error', err);
-      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-        handleDisconnect();                         // lost due to either server restart, or a
-      } else {                                      // connnection idle timeout (the wait_timeout
-        throw err;                                  // server variable configures this)
-      }
-    });
+function handleDisconnect(dbMain){
+  dbMain.on('error', function(err){
+
+    console.log('\nRe-connecting lost connection: ' + err.stack);
+    dbMain.destroy();
+
+    dbMain = mysql.createConnection(mysqlConfig);
+    handleDisconnect(connection);
+    dbMain.connect();
+
+  });
 }
 
-handleDisconnect();
+handleDisconnect(dbMain);
+
+// dbMain.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Database connected!");
+// });
+
+// function handleDisconnect() {
+//   dbMain.connect(function(err) {
+//     if(err) {
+//       console.log('Error when connecting to db:', err);
+//       setTimeout(handleDisconnect, 2000);
+//     }
+//     console.log("Database connected!");
+//   });
+//
+//   dbMain.on('error', function(err) {
+//     console.log('db error', err);
+//     if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+//       return handleDisconnect();
+//     } else {
+//       throw err;
+//     }
+//   });
+// }
+//
+// handleDisconnect();
 
 /* ------- Section for developing pages ------- */
 
