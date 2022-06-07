c0f09b00d5b81c35c9951b232bd4faec5a66c7fe
syaedisyafrilnizam <60510081+syaedii@users.noreply.github.com>
2022-06-07 11:39:32 +0900

    DB connection handler

--- server.js
+++ server.js
@@ -51,10 +51,27 @@ const exam = require('./routes/exam');
 
 //Checking DB conneciton
 db = connection.db;
-db.connect(function(err) {
-  if (err) throw err;
-  console.log("Database connected!");
-});
+
+function handleDisconnect() {
+  db.connect(function(err) {
+    if(err) {
+        console.log('error when connecting to db: ', err);
+        setTimeout(handleDisconnect, 2000);
+      }
+      console.log("Database connected!");
+    });
+
+    db.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        handleDisconnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+    });
+}
+
+handleDisconnect();
 
 /* ------- Section for developing pages ------- */
 
